62074ca51e22191e35a64907b572a2375ef0c056
waitingmyself <waitingmyself@163.com>
2012-10-12 17:58:35 +0800

    首页修改

--- static/jsonp.js
+++ static/jsonp.js
@@ -1,34 +1,38 @@
 function addMeshSearch() {
-	var source = function( request, response ) {
-		$.ajax({
-			url: "http://192.168.8.14:3000/jsonp/" + request.term,
-			dataType: "jsonp",
-			success: function( data ) {
-				response($.map( data && data.data || [], function(item ) {
-				var label = item.DescriptorName.String + '[' + (item.zh&&item.zh.String) + ']';
-				var tree = '';
-				for(var i = 0; i < item.TreeNumberList.length;i++) {
-					tree = tree + '<br>' + item.TreeNumberList[i] + ' '
+	var source = function(request, response) {
+			$.ajax({
+				url: "http://192.168.8.14:3000/jsonp/" + request.term,
+				dataType: "jsonp",
+				success: function(data) {
+					response($.map(data && data.data || [], function(item) {
+						var label = item.DescriptorName.String + '[' + (item.zh && item.zh.String) + ']';
+						var tree = '';
+						for(var i = 0; i < item.TreeNumberList.length; i++) {
+							tree = tree + '<br>' + item.TreeNumberList[i] + ' '
+						}
+						return {
+							label: label,
+							value: item.DescriptorName.String,
+							desc: tree
+						}
+					}));
 				}
-					return {
-						label: label,
-						value: item.DescriptorName.String,
-						desc:tree
-					}
-				}));
-			}
-		});
-	}
+			});
+		}
 	$('#jsonp').autocomplete({
-				source:source,
-				minLength: 2
-			}).data( "autocomplete" )._renderItem = function( ul, item ) {
+		source: source,
+		minLength: 2,
+		select: function(event, ui) {
+			$('a').html(ui.item.value).attr('href','tree/' + ui.item.value);
+		}
+	});
+	/*.data( "autocomplete" )._renderItem = function( ul, item ) {
 				return $( "<li></li>" )
 					.data( "item.autocomplete", item )
 					.append( "<a>" + item.label + "<span style=\"color:orange;\">" + item.desc + "</span></a>" )
 					.appendTo( ul );
-			};
+			};*/
 }
-$(function(){
+$(function() {
 	addMeshSearch();
 });
